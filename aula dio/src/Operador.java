ae34a34fcf1309110a2151eb7d25d53a9fc394df
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-08-28 17:06:57 -0300

    adicionando os valor de inteiro e boolean

--- aula dio/src/Operador.java
+++ aula dio/src/Operador.java
@@ -1,14 +1,24 @@
 public class Operador {
     public static void main(String[] args) {
         int numero = 5;
-
+        boolean variaveis = true;
+        //--------------------------------------------------------------------------
+        //Altera o valos de positivo para negativo 
         numero = -numero;
-
-        System.out.println(numero);
+        System.out.println(numero);// valores negativo
 
         numero = numero * - 1;
-        System.out.println(numero);
-
+        System.out.println(numero); // valor positivos
+    // -----------------------------------------------------------------------------
+        //como almenta o valor sem fazer alteraçõa na variavel inicial
+        numero = numero + 1 ;
+        System.out.println(numero); //almentarndo o valor em mas 1 
+        
+//-----------------------------------------------------------------------------------
+// vareavel boolean
+        variaveis = !variaveis; //deixar negativo
+    
+        System.out.println(variaveis);
         
     }
 }
---
+++ aula dio/src/Operador1.java
@@ -0,0 +1,12 @@
+public class Operador1 {
+    public static void main(String[] args) {
+       
+    boolean variaveis = true;
+
+    variaveis = !variaveis; //deixar negativo
+
+    System.out.println(variaveis);
+
+
+     }
+}
