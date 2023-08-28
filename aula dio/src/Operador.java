783248e3292852723a204771e3df7065757f77b4
kauan-17 <83840682+kauan-17@users.noreply.github.com>
2023-08-28 17:21:16 -0300

    Update Operador.java

--- aula dio/src/Operador.java
+++ aula dio/src/Operador.java
@@ -10,12 +10,12 @@ public class Operador {
         numero = numero * - 1;
         System.out.println(numero); // valor positivos
     // -----------------------------------------------------------------------------
-        //como almenta o valor sem fazer alteraçõa na variavel inicial
+        //como aumenta o valor sem fazer alteraçõa na variavel inicial
         numero = numero + 1 ;
         System.out.println(numero); //almentarndo o valor em mas 1 
         
 //-----------------------------------------------------------------------------------
-// vareavel boolean
+// variável booleana
         variaveis = !variaveis; //deixar negativo
     
         System.out.println(variaveis);
