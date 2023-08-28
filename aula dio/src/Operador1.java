b3f20dbb55d4cdcac90b50c82a36ddf1e4f7aeb0
kauan-17 <83840682+kauan-17@users.noreply.github.com>
2023-08-28 17:39:35 -0300

    Update Operador1.java

--- aula dio/src/Operador1.java
+++ aula dio/src/Operador1.java
@@ -1,12 +1,23 @@
 public class Operador1 {
     public static void main(String[] args) {
-       
-    boolean variaveis = true;
+       int a , b;
 
-    variaveis = !variaveis; //deixar negativo
+       a=5;
+       b=6;
+        String resultado = a==b ?"verdadeiro": "falso";
+        System.out.println(resultado);
+        //valores do tipo inteiro
+        int resultado1 = a==b ? 1 : 0;
+        System.out.println(resultado1);
 
-    System.out.println(variaveis);
-
+   /*EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE sem o valor de boolean
+       if (a==b){
+        resultado = "verdadeiro";
+    }
+       else{
+        resultado = "falso";
+         System.out.println(resultado);
+    }*/
 
      }
 }
