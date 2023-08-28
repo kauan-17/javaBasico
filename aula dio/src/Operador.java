b6d6a65ec3b81ffe4682bb6df0650b37bd71d5b8
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-08-28 17:36:36 -0300

    resolvendo

--- aula dio/src/Operador.java
+++ aula dio/src/Operador.java
@@ -2,31 +2,19 @@ public class Operador {
     public static void main(String[] args) {
         int numero = 5;
         boolean variaveis = true;
-        //--------------------------------------------------------------------------
-        //Alterar o valor de positivo para negativo
+    //--------------------------------------------------------------------------
+    //Alterar o valor de positivo para negativo
         numero = -numero;
         System.out.println(numero);// valor negativo
         numero = numero * - 1;
         System.out.println(numero); // valor positivos
     // -----------------------------------------------------------------------------
-<<<<<<< HEAD
-
-        //ccomo aumentar o valor sem fazer alteração na variavel inicial
-=======
-       
-        //como aumentar o valor sem fazer alteração na variavel inicial
->>>>>>> 937b5e035e491cc7f89eb3eead31c2bf29916341
+    //ccomo aumentar o valor sem fazer alteração na variavel inicial
         numero = numero + 1 ;
-        System.out.println(numero); //almentarndo o valor em mas 1 
-        
+        System.out.println(numero); //almentarndo o valor em mas 1        
     //-------------------------------------------------------------------------------
     // variável booleana
-<<<<<<< HEAD
-
-=======
->>>>>>> 937b5e035e491cc7f89eb3eead31c2bf29916341
         variaveis = !variaveis; //deixar negativo
-    
         System.out.println(variaveis);
         
     }
