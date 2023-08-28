1bf0ea953bcbecd2ff063d9864ee3d98a4ba3380
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-08-28 15:21:26 -0300

    adicionar os tipo de variaveis de ambiente.

---
+++ aula dio/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+  public static void main(String[] args) {
+    double salarioMinimo = 2500.33;
+    byte idade = 35;
+    short ano = 2024;
+    int cep = 21070333;
+    long cpf = 16658754160L;
+    float pi = 3.14F;
+
+  }  
+}
