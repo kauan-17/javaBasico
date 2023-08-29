7a91ed47666476d57b3d6a65531465a9a8f7baa8
Kauan Oliveira das neves <Kauan17neves@gmail.com>
2023-08-29 10:10:18 -0300

    colacando o java docs

---
+++ aula dio/exemplo das aulas/src/Comentario.java
@@ -0,0 +1,18 @@
+public class Comentario {
+    
+public static void main(String[] args) {
+    
+}
+        public int somaMultiplica (int n, int x, String m){
+            int r = 0; //r e igual ao resultado
+            if(m == "M"){ //M = multiplcação
+                r= n *x;
+            }
+                else{
+                    //se não soma mesmo
+                    r= n + x;
+                }
+                return r;
+
+        }
+}
